0c50ea0f5245fe5bd1d5fd5cacd834a7d0996f70
Albeiro Marin <xlandru@hotmail.com>
2021-06-26 14:50:40 -0500

    Fix products endpoint offset issues

--- src/api/productsMobile/controllers.js
+++ src/api/productsMobile/controllers.js
@@ -4,7 +4,7 @@ function getProductsByFamily(request, response) {
   pool.query(
     `SELECT * FROM ecommerce.products
          WHERE productType='${request.params.family}'  
-         LIMIT 2 OFFSET ${request.params.offset}`,
+         LIMIT 8 OFFSET ${request.params.offset}`,
     (error, results) => {
       if (error) response.send(error.message);
       response.send(results);
